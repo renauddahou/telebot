bb403963492d2f8a6159ce93431e37e41b3223da
Yuvarajamaster <52627980+Yuvarajamaster@users.noreply.github.com>
2021-07-11 20:17:05 +0530

    Update telebotpy.py
    added animation gif

--- telebotpy.py
+++ telebotpy.py
@@ -17,8 +17,10 @@ fan_off = ["turn off the fan","turn off fan","fan off","off the fan"]
 def ledon(bot,update):
   chat_id = bot.message.chat_id
   path='https://cdn3.vectorstock.com/i/1000x1000/52/87/abstract-lightbulb-vector-955287.jpg'
-  bot.message.reply_text("Done,lights turned on!💡")
+  animation_url = 'https://i.gifer.com/H90l.gif'
+  bot.message.reply_text("Done,lights turned on!")
   update.bot.sendPhoto(chat_id=chat_id,photo=path)
+  update.bot.sendAnimation(chat_id=chat_id,animation=animation_url)
   aio.send('led', 1)
   
 def ledoff(bot,update):
