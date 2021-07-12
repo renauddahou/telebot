6b921f8acf20057157dd1952ebdacc858f5cc3e4
Yuvarajamaster <52627980+Yuvarajamaster@users.noreply.github.com>
2021-07-12 12:25:24 +0530

    Update telebotpy.py

--- telebotpy.py
+++ telebotpy.py
@@ -1,51 +1,56 @@
-from Adafruit_IO import Client
-from telegram.ext import Updater,MessageHandler,Filters
-import os
-import random 
-feed_key = os.getenv('feed_key')
+from Adafruit_IO import Client                           #importing adafruit library
+from telegram.ext import Updater,MessageHandler,Filters  #importing telegram libraries
+import os                                                #import os to get hidden keys
+import random
+#get feed key of adafruit feed
+feed_key = os.getenv('feed_key') 
+#creating a client
+aio = Client('uvi', feed_key)  
 
-aio = Client('uvi', feed_key)  #for light
 
-
-
+#lists 
 greeting = ["hi","hello","hey"]
-tay = ["what can you do","what will you do","say about yourself"]
-howlist = ["how are you"]
+tay = ["what can you do?","what will you do?","say about yourself"]
+howlist = ["how are you?","how are you"]
 finelist = ["fine","great","good"]
 light_on = ["turn on the light","turn on light","lights on","light on","its dark here","on the light"]
 light_off = ["turn off the light","turn off light","lights off","light off"]
 fan_on = ["turn on the fan","turn on fan","fan on","on the fan"]
 fan_off = ["turn off the fan","turn off fan","fan off","off the fan"]
 
-
+#to make the light ON
 def ledon(bot,update):
   chat_id = bot.message.chat_id
   animation_url = 'https://media.baamboozle.com/uploads/images/68811/1618179100_34871_gif-url.gif'
   bot.message.reply_text("Done,lights turned on!✌")
   update.bot.sendAnimation(chat_id=chat_id,animation=animation_url,duration=2)
   aio.send('led', 1)
-  
+
+#to make the light OFF
 def ledoff(bot,update):
   chat_id = bot.message.chat_id
   path='https://labblog.uofmhealth.org/sites/lab/files/2018-11/michigan-med-l-ocd-study.gif'
   bot.message.reply_text("Done,lights turned off!👍")
   update.bot.sendAnimation(chat_id=chat_id,animation=path)
   aio.send('led', 0)
-  
+
+#make fan ON
 def fanon(bot,update):
   chat_id = bot.message.chat_id
   path='https://mir-s3-cdn-cf.behance.net/project_modules/disp/bafb3929035897.55decb26f207b.gif'
   bot.message.reply_text("Done,fan turned on!✌")
   update.bot.sendAnimation(chat_id=chat_id,animation=path)
   aio.send('fan', 1)
-  
+
+#make fan OFF
 def fanoff(bot,update):
   chat_id = bot.message.chat_id
   path='https://d1j8pt39hxlh3d.cloudfront.net/uploads/thumbs_up_sign_256_1.gif'
   bot.message.reply_text("Done,fan turned off!")
   update.bot.sendAnimation(chat_id=chat_id,animation=path)
   aio.send('fan', 0)
-  
+
+#to get the status of light
 def lightOnorOff(bot,update):
   feedLight = aio.receive('led')
   if(feedLight.value==1):
@@ -54,7 +59,7 @@ def lightOnorOff(bot,update):
     bot.message.reply_text("OFF")
   print(feedLight.value)
   
-    
+#get status of fan    
 def fanOnorOff(bot,update):
   feedFan = aio.receive('fan')
   if(feedFan.value==1):
@@ -63,15 +68,16 @@ def fanOnorOff(bot,update):
     bot.message.reply_text("OFF")
   print(feedFan.value)
   
-  
+#for invalid commands  
 def inval(bot,update): 
   bot.message.reply_text("Invalid command!")
 
+#greeting 
 def greet(bot,update):
-  bot.message.reply_text("👋Hi!.. You can give commands to turn on or off the light and fan and to get status of light and fan")
+  bot.message.reply_text("👋Hi!.. You can give me commands to turn ON or OFF the light or fan and also to get status of light and fan.")
   
     
-  
+#about the bot  
 def about(bot,update):
   bot.message.reply_text("I can turn on and off the light and fan for you")
 
